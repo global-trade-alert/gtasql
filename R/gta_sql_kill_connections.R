b4cef9c43985de4e2f70892e579ef700b47efbea
Johannes <fritz.johannes@gmail.com>
2019-09-02 22:13:53 +0200

    function to close all open db connections

---
+++ R/gta_sql_kill_connections.R
@@ -0,0 +1,25 @@
+# Roxygen documentation
+
+#' Kill all open connections to a remote database.
+#'
+#' Out they go
+
+#' @references www.globaltradealert.org
+#' @author Global Trade Alert
+
+
+gta_sql_append_table <- function() {
+  ## taken from https://stackoverflow.com/users/2610541/thankgoat at https://stackoverflow.com/questions/32139596
+  
+  all_cons <- dbListConnections(MySQL())
+  
+  print(all_cons)
+  
+  for(con in all_cons)
+    +  dbDisconnect(con)
+  
+  print(paste(length(all_cons), " connections killed."))
+  
+  
+}
+
