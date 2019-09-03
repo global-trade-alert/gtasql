ed3470c07bb4c700cb2a7846d99cb27fc81ad1c4
Johannes <fritz.johannes@gmail.com>
2019-09-03 15:36:46 +0200

    counting connections to find leaks

---
+++ R/gta_sql_count_connections.R
@@ -0,0 +1,20 @@
+# Roxygen documentation
+
+#' Count all open connections to a remote database.
+#'
+#' Here you go.
+
+#' @references www.globaltradealert.org
+#' @author Global Trade Alert
+
+
+gta_sql_count_connections <- function() {
+  ## taken from https://stackoverflow.com/users/2610541/thankgoat at https://stackoverflow.com/questions/32139596
+  library(RMySQL)
+  
+  all_cons <- dbListConnections(MySQL())
+  
+  return(length(all_cons))
+  
+}
+
