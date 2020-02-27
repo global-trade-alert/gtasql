27c11a1f02d4588578b753d84076b910da95231c
Johannes Fritz <fritz.johannes@gmail.com>
2020-02-27 09:41:24 +0100

    init

---
+++ R/gta_upload_to_aws.R
@@ -0,0 +1,33 @@
+# Roxygen documentation
+
+#' Upload a file into our AWS S3 account and return its  public URL.
+#'
+#' @return An AWS url.
+#' @references www.globaltradealert.org
+#' @author GTA
+
+
+# Function infos and parameters  --------------------------------------------
+
+gta_upload_to_aws = function(upload.file.name=NULL,
+                             upload.file.path=NULL,
+                             upload.destination="gtaupload/Uploads/files/",
+                            credential.path="setup/keys/as3.R"){
+  
+  ## For how to interact with AWS from the command line, see https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
+  
+  source(credential.path)
+  
+  upload.location=gsub("/+","/",paste0(upload.file.path,"/",upload.file.name))
+  
+  system(paste0(aws.cred," aws s3 cp '",upload.location,"' s3://",upload.destination," --acl public-read"))
+  
+  
+  aws.url=paste0("http://s3-eu-west-1.amazonaws.com/",gsub("/+","/",paste0(upload.destination,"/",upload.file.name)))
+  
+  
+  return(aws.url)
+}
+
+
+
