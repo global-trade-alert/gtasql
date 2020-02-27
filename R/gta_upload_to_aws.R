9e12fcb7b8cd6cfec93b9aab031f59546d438e48
Johannes Fritz <fritz.johannes@gmail.com>
2020-02-27 11:18:07 +0100

    more robust file paths.

--- R/gta_upload_to_aws.R
+++ R/gta_upload_to_aws.R
@@ -17,13 +17,18 @@ gta_upload_to_aws = function(upload.file.name=NULL,
   ## For how to interact with AWS from the command line, see https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2.html
   
   source(credential.path)
+  file.name.nogap=gsub("\\s+","_",upload.file.name)
   
-  upload.location=gsub("/+","/",paste0(upload.file.path,"/",upload.file.name))
+  upload.location=gsub("/+","/",paste0(upload.file.path,"/",file.name.nogap))
+  
+  file.copy(gsub("/+","/",paste0(upload.file.path,"/",upload.file.name)), 
+                   upload.location)
   
   system(paste0(aws.cred," aws s3 cp '",upload.location,"' s3://",upload.destination," --acl public-read"))
   
+  file.remove(upload.location)
   
-  aws.url=paste0("http://s3-eu-west-1.amazonaws.com/",gsub("/+","/",paste0(upload.destination,"/",upload.file.name)))
+  aws.url=paste0("http://s3-eu-west-1.amazonaws.com/",gsub("/+","/",paste0(upload.destination,"/",file.name.nogap)))
   
   
   return(aws.url)
